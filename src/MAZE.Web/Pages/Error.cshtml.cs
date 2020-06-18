88b2306d3a8a393675b20a5407e856f67c52a18d
Philip Wallin <wallin_philip@hotmail.com>
2020-06-18 14:50:30 +0200

    Added project for persisting.

--- src/MAZE.Web/Pages/Error.cshtml.cs
+++ src/MAZE.Web/Pages/Error.cshtml.cs
@@ -1,25 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace MAZE.Web.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+#pragma warning disable SA1649 // File name should match first type name
     public class ErrorModel : PageModel
+#pragma warning restore SA1649 // File name should match first type name
     {
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
